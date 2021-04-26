3a3b9d3261af64f37a377e574840f05fae45ca41
doducchien <chiendd226@gmail.com>
2021-04-26 23:56:07 +0700

    1

--- frontend-app/src/components/staff/ListStaff.jsx
+++ frontend-app/src/components/staff/ListStaff.jsx
@@ -23,10 +23,12 @@ import * as constraints from '../../constraints'
 //export excel
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
-import { CSVLink } from 'react-csv'
+
 
 
 import ReactExport from 'react-data-export';
+import ReactHTMLTableToExcel from 'react-html-table-to-excel';
+
 
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
@@ -50,56 +52,7 @@ function createData(image, email, fullName, phoneNumber, sex, joinDay, leftDay, 
 
 
 
-const TableStaff = ({ listStaff, openDetailStaff }) => {
 
-
-    return (
-        <TableContainer style={{ width: '100%' }} component={Paper}>
-            <Table aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Ảnh</TableCell>
-                        <TableCell align="center">Email</TableCell>
-                        <TableCell align="center">Họ Tên</TableCell>
-                        <TableCell align="center">Số điện thoại</TableCell>
-                        <TableCell align="center">Giới tính</TableCell>
-                        <TableCell align="center">Ngày tham gia</TableCell>
-                        <TableCell align="center">Ngày nghỉ việc</TableCell>
-                        <TableCell align="center">Vị trí</TableCell>
-                        <TableCell align="center">Hành động</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {listStaff.map((item) => {
-                        let { image, email, fullName, phoneNumber, sex, joinDay, leftDay, position } =
-                            createData(item.image, item.email, item.fullName, item.phoneNumber, item.sex, item.joinDay, item.leftDay, item.position)
-                        return (
-
-                            <TableRow key={email}>
-                                <TableCell component="th" scope="row">
-                                    <img style={{ width: '50px', height: '50px', borderRadius: '50%' }} src={image} />
-                                </TableCell>
-
-                                <TableCell align="center">{email}</TableCell>
-                                <TableCell align="center">{fullName}</TableCell>
-                                <TableCell align="center">{phoneNumber}</TableCell>
-                                <TableCell align="center">{sex}</TableCell>
-                                <TableCell align="center">{joinDay}</TableCell>
-                                <TableCell align="center">{leftDay}</TableCell>
-                                <TableCell align="center">{position}</TableCell>
-                                <TableCell align="center" onClick={() => openDetailStaff(email)} align="right"><ArrowRightAltIcon /></TableCell>
-
-
-                            </TableRow>
-                        )
-                    }
-
-                    )}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
-}
 
 function ListStaff(props) {
 
@@ -108,66 +61,118 @@ function ListStaff(props) {
     const { user_role, listStaff, openDetailStaff } = props
     const DataSet = [
         {
+            value: 'hihihi',
+            xSteps: 1, // Will start putting cell with 1 empty cell on left most
+            ySteps: 5, //will put space of 5 rows,
+
             columns: [
-                { title: "Ảnh", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
-                { title: "Eamil", style: { font: { sz: "18", bold: true } }, width: { wch: 30 } }, // width in characters
-                { title: "Họ và tên", style: { font: { sz: "18", bold: true } }, width: { wpx: 100 } }, // width in pixels
-                { title: "Số điện thoại", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
-                { title: "Giới tính", style: { font: { sz: "18", bold: true } }, width: { wpx: 100 } }, // width in pixels
-                { title: "Ngày tham gia ", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
-                { title: "Ngày nghỉ việc", style: { font: { sz: "18", bold: true } }, width: { wch: 30 } }, // width in characters
-                { title: "Vị trí", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
+                { title: "ẢNH", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
+                { title: "EMAIL", style: { font: { sz: "18", bold: true } }, width: { wch: 30 } }, // width in characters
+                { title: "HỌ VÀ TÊN", style: { font: { sz: "18", bold: true } }, width: { wpx: 120 } }, // width in pixels
+                { title: "SỐ ĐIỆN THOẠI", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
+                { title: "GIỚI TÍNH", style: { font: { sz: "18", bold: true } }, width: { wpx: 100 } }, // width in pixels
+                { title: "NGÀY THAM GIA ", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
+                { title: "NGÀY NGHỈ VIỆC", style: { font: { sz: "18", bold: true } }, width: { wch: 30 } }, // width in characters
+                { title: "VỊ TRÍ", style: { font: { sz: "18", bold: true } }, width: { wpx: 125 } }, // width in pixels
 
 
             ],
-            // data: listStaff.map((data) => [
-            //     { value: data.image, style: { font: { sz: "14" } } },
-            //     { value: data.email, style: { font: { sz: "14" } } },
-            //     { value: data.fullName, style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "3461eb" } } } },
-            //     { value: data.phoneNumber, style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "eb1207" } } } },
-            //     { value: data.sex, style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "4bd909" } } } },
-            //     { value: data.joinDay, style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "ebc907" } } } },
-            //     { value: data.leftDay, style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "35bdb4" } } } },
-            //     { value: data.position, style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "ed14f5" } } } },
+            data: listStaff.map((data) => {
+                let { image, email, fullName, phoneNumber, sex, joinDay, leftDay, position } = data || ''
+                if (sex === 0) sex = 'nam'
+                else if (sex === 1) sex = 'nữ'
+                else sex = 'khác'
+                joinDay = constraints.changeIntToTime(parseInt(joinDay))
 
-            // ])
-            data: listStaff.map((data) => [
-                { value: 'image', style: { font: { sz: "14" } } },
-                { value: 'email', style: { font: { sz: "14" } } },
-                { value: 'fullName', style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "3461eb" } } } },
-                { value: 'phoneNumber', style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "eb1207" } } } },
-                { value: 'sex', style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "4bd909" } } } },
-                { value: 'joinDay', style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "ebc907" } } } },
-                { value: 'leftDay', style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "35bdb4" } } } },
-                { value: 'position', style: { font: { color: { rgb: "ffffff" } }, fill: { patternType: "solid", fgColor: { rgb: "ed14f5" } } } },
+                if (leftDay !== null) leftDay = constraints.changeIntToTime(parseInt(leftDay))
+                else leftDay = 'Chưa có'
+                return [
+                    { value: image || 'null', style: { font: { sz: "14" } } },
+                    { value: email || 'null', style: { font: { sz: "14" } } },
+                    { value: fullName || 'null', style: { font: { sz: "14" } }, fill: { patternType: "solid", fgColor: { rgb: "3461eb" } } },
+                    { value: phoneNumber || 'null', fill: { patternType: "solid", fgColor: { rgb: "eb1207" } } },
+                    { value: sex || 'null', fill: { patternType: "solid", fgColor: { rgb: "4bd909" } } },
+                    { value: joinDay || 'null', fill: { patternType: "solid", fgColor: { rgb: "ebc907" } } },
+                    { value: leftDay || 'null', fill: { patternType: "solid", fgColor: { rgb: "35bdb4" } } },
+                    { value: position || 'null', fill: { patternType: "solid", fgColor: { rgb: "ed14f5" } } },
 
-            ])
-        }
+                ]
+            }),
+
+
+        },
+
     ]
-    console.log(DataSet)
-
-    const dataExcel = listStaff.map(item => {
-        const { image, email, fullName, phoneNumber, sex, joinDay, leftDay, position } = item
-        return createData(image, email, fullName, phoneNumber, sex, joinDay, leftDay, position)
-
-    })
+    const TableStaff = ({ listStaff, openDetailStaff }) => {
 
 
+        return (
+            <div className='list-staff'>
+                <TableContainer style={{ width: '100%' }} component={Paper}>
+                    <Table id='export'>
+                        <TableHead>
+                            <TableRow >
+                                <TableCell  align="center" colSpan={8} >Báo cáo danh sách nhân viên công ty TNHH dịch vụ công nghệ Thiên Hằng</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                {/* <TableCell>Ảnh</TableCell> */}
+                                <TableCell align="center">Email</TableCell>
+                                <TableCell align="center">Họ Tên</TableCell>
+                                <TableCell align="center">Số điện thoại</TableCell>
+                                <TableCell align="center">Giới tính</TableCell>
+                                <TableCell align="center">Ngày tham gia</TableCell>
+                                <TableCell align="center">Ngày nghỉ việc</TableCell>
+                                <TableCell align="center">Vị trí</TableCell>
+                                <TableCell align="center">Hành động</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {listStaff.map((item) => {
+                                let { image, email, fullName, phoneNumber, sex, joinDay, leftDay, position } =
+                                    createData(item.image, item.email, item.fullName, item.phoneNumber, item.sex, item.joinDay, item.leftDay, item.position)
+                                return (
 
-    const exportToCSV = (csvData, fileName) => {
+                                    <TableRow key={email}>
+                                        {/* <TableCell component="th" scope="row">
+                                            <img style={{ width: '50px', height: '50px', borderRadius: '50%' }} src={image} />
+                                        </TableCell> */}
+
+                                        <TableCell align="center">{email}</TableCell>
+                                        <TableCell align="center">{fullName}</TableCell>
+                                        <TableCell align="center">{phoneNumber}</TableCell>
+                                        <TableCell align="center">{sex}</TableCell>
+                                        <TableCell align="center">{joinDay}</TableCell>
+                                        <TableCell align="center">{leftDay}</TableCell>
+                                        <TableCell align="center">{position}</TableCell>
+                                        <TableCell align="center" onClick={() => openDetailStaff(email)} align="right"><ArrowRightAltIcon /></TableCell>
 
 
-        const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-        const fileExtension = '.xlsx';
-        const ws = XLSX.utils.json_to_sheet(csvData, {
-            header: ['hihi'],
-        });
-        const wb = { Sheets: { 'data': ws }, SheetNames: ['data'] };
-        const excelBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
-        const data = new Blob([excelBuffer], { type: fileType });
-        FileSaver.saveAs(data, fileName + fileExtension);
+                                    </TableRow>
+                                )
+                            }
+
+                            )}
+                            <TableRow >
+                                <TableCell  align="center" colSpan={8} >s</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+
+                </TableContainer>
+                <ReactHTMLTableToExcel
+                    table="export"
+                    filename="Báo cáo danh sách nhân viên"
+                    sheet="tablexls"
+                    buttonText='Xuất báo cáo'
+                    className='export'
+
+                />
+            </div>
+
+        )
     }
 
+    console.log(listStaff)
     return (
         <div className="list-staff">
 
@@ -175,8 +180,7 @@ function ListStaff(props) {
             <TableStaff listStaff={listStaff} openDetailStaff={openDetailStaff} />
 
 
-            <div className='right'>
-                {/* <Button onClick={() => exportToCSV(dataExcel, 'listStaff')} style={{ height: '70px' }} variant="outlined" color="secondary">Xuất danh sách nhân viên</Button> */}
+            {/* <div className='right'>
                 {
                     listStaff.length > 0 ?
                         <ExcelFile
@@ -184,11 +188,11 @@ function ListStaff(props) {
                             element={<button type="button" >Export Data</button>}>
                             <ExcelSheet dataSet={DataSet} name="Covid-19 Country Report" />
                         </ExcelFile>
-                    :
-                    ''
+                        :
+                        ''
                 }
 
-            </div>
+            </div> */}
 
 
         </div>
