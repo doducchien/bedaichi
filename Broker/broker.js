683b00617ed9f8ceefc2cb2f6c23a9d2b138f41e
doducchien <chiendd226@gmail.com>
2021-04-28 05:42:43 +0700

    1

--- Broker/broker.js
+++
@@ -1,8 +0,0 @@
-var aedes = require('aedes')();
-
-var server = require('net').createServer(aedes.handle);
-var port = 1883;
-
-server.listen(port, function(){
-    console.log('Broker is listening on port ' + port);
-})
